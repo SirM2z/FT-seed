19f5da6354b81e8e82beff0a719d4f2c45fc4c9f
sirm2z <sirm2zisv@gmail.com>
2019-08-25 11:17:02 +0800

    fix: get award

--- index.js
+++ index.js
@@ -32,8 +32,10 @@ const DAILY_TASK_GET_JF_ENABLE_SELECTOR = '#app > div > div.sign-in-com.fixed > 
 const DAILY_TASK_GET_JF_DISABLE_SELECTOR = '#app > div > div.sign-in-com.fixed > div.sign-in > div.sign-info > div.sign-btn.bg_orange_op2.c_orange';
 // 每日任务 签到面板关闭按钮
 const DAILY_TASK_SIGN_MODAL_CLOSE_SELECTOR = '#app > div > div.sign-in-com.fixed > div.sign-in > div.icon-close-o';
-// 每日任务 7日抽奖按钮
-const DAILY_TASK_7_AWARD_SELECTOR = '#app > div > div.sign-in-com.fixed > div.sign-in > div.sign-list > ul > li.no-select.bg_gray.c_deep_gray > i';
+// 每日任务 7日抽奖按钮--不可抽
+const DAILY_TASK_7_AWARD_DISABLE_SELECTOR = '#app > div > div.sign-in-com.fixed > div.sign-in > div.sign-list > ul > li.no-select.bg_gray.c_deep_gray > i';
+// 每日任务 7日抽奖按钮--可抽
+const DAILY_TASK_7_AWARD_ENABLE_SELECTOR = '#app > div > div.sign-in-com.fixed > div.sign-in > div.sign-list > ul > li.no-select.bg_orange_op2.c_orange > i';
 // 退出页
 const LOGOUT = 'https://account.futu5.com/user/logout';
 // #endregion
@@ -300,8 +302,16 @@ const sign = async (browser, page) => {
     console.log(`    已经签到`);
   }
   console.log(`    开始抽取7日奖励`);
-  await page.click(DAILY_TASK_7_AWARD_SELECTOR);
-  console.log(`    以点击抽奖按钮`);
+  let judgeIsAward = await Promise.race([
+    page.waitForSelector(DAILY_TASK_7_AWARD_ENABLE_SELECTOR, {visible: true}).then(_ => 1),
+    page.waitForSelector(DAILY_TASK_7_AWARD_DISABLE_SELECTOR, {visible: true}).then(_ => 2)
+  ]);
+  if (judgeIsAward === 1) {
+    await page.click(DAILY_TASK_7_AWARD_ENABLE_SELECTOR);
+    console.log(`    成功抽奖`);
+  } else {
+    console.log(`    不能抽奖`);
+  }
   await page.click(DAILY_TASK_SIGN_MODAL_CLOSE_SELECTOR);
   console.log(`    开始领取奖励`);
   await page.click(DAILY_TASK_FIRST_SELECTOR);
